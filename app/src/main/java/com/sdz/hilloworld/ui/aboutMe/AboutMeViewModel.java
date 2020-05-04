4f07a3633a3d8d8bdeb36e7dcc75dcb53c0ec114
sdz630 <sdz630@outlook.com>
2020-05-04 14:41:06 +0800

    Revert "version1.0Beta"
    This reverts commit f96664c51f0499ea7812d6b3f0d693f18b2a2aba.

--- app/src/main/java/com/sdz/hilloworld/ui/aboutMe/AboutMeViewModel.java
+++
@@ -1,6 +0,0 @@
-package com.sdz.hilloworld.ui.aboutMe;
-
-import androidx.lifecycle.ViewModel;
-
-public class AboutMeViewModel extends ViewModel {
-}
--- app/src/main/java/com/sdz/hilloworld/ui/home/tools/ToolsViewModel.java
+++
@@ -1,6 +0,0 @@
-package com.sdz.hilloworld.ui.home.tools;
-
-import androidx.lifecycle.ViewModel;
-
-public class ToolsViewModel extends ViewModel {
-}
